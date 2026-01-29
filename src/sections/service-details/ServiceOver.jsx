cacacfa16adbbfba7c8e50ccdc10e9e050edbf59
manishadtanii <Manish@digicots.com>
2026-01-29 08:23:24 -0500

    changes done on frontenbd on verview sectionddd

--- src/sections/service-details/ServiceOver.jsx
+++ src/sections/service-details/ServiceOver.jsx
@@ -4,12 +4,7 @@ import { motion } from "framer-motion";
 const ServiceOver = ({ data }) => {
    if (!data) return null;
   console.log("Service Over data:", data);
-  const { heading, img, para, title } = data;
-  console.log(title);
-  console.log(para);
-  console.log(img);
-  console.log(heading);
-
+  const { heading, img, para1, para2, title } = data;
   return (
     <section className="bg-white">
       <div className="container-fluid">
@@ -67,17 +62,17 @@ const ServiceOver = ({ data }) => {
                 viewport={{ once: true }}
                 transition={{ duration: 0.6, delay: 0.3  }}
               >
-                {para}
+                {para1}
               </motion.p>
-            {/* <motion.p
+            <motion.p
                 className="md:mt-6 text-gray-700 text-base leading-relaxed"
                 initial={{ opacity: 0, y: 20 }}
                 whileInView={{ opacity: 1, y: 0 }}
                 viewport={{ once: true }}
                 transition={{ duration: 0.6, delay: 0.3 }}
               >
-                {para}
-              </motion.p> */}
+                {para2}
+              </motion.p>
             </div>
           </motion.div>
         </div>
